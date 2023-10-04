4a8241b15796bc301325e6b660aeaab646c08408
Mohammadreza Khoshghamat <58995528+MrKhoshghamat@users.noreply.github.com>
2023-10-05 00:14:58 +0330

    BaseEntity and Generic Repository Interface Added

---
+++ Cucina.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Cucina.Domain.Entities.Base
+{
+    public class BaseEntity<TPrimaryKey>
+    {
+        #region Properties
+
+        public TPrimaryKey Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+        #endregion
+    }
+}
