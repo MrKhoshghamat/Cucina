69846569fbc929df67b3a121219d7676c9134332
sjmostafania <s.j.mostafania@gmail.com>
2023-10-19 23:12:31 +0330

    Update user feature has been added

---
+++ Cucina.Application/Features/User/Commands/Update/Handler/UpdateUserCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Cucina.Application.Features.Common;
+using Cucina.Application.Features.User.Commands.Update.Validation;
+using Cucina.Domain;
+using MediatR;
+
+namespace Cucina.Application;
+
+public class
+    UpdateUserCommandHandler : IRequestHandler<UpdateUserCommandRequest, HandlerResponse<UpdateUserCommandResponse>>
+{
+    #region privateMember
+
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    #endregion
+
+    #region Consructor
+
+    public UpdateUserCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    #endregion
+
+    #region Handler
+
+    public async Task<HandlerResponse<UpdateUserCommandResponse>> Handle(UpdateUserCommandRequest request,
+        CancellationToken cancellationToken)
+    {
+        var handlerResponse = new HandlerResponse<UpdateUserCommandResponse>();
+        var validationRules = new UpdateUserValidator(_unitOfWork, _mapper);
+        var validationResult = await validationRules.ValidateAsync(request, cancellationToken);
+
+        if (!validationResult.IsValid)
+        {
+            handlerResponse.IsSucceed = false;
+            handlerResponse.Data = _mapper.Map<UpdateUserCommandResponse>(request);
+            handlerResponse.Message = validationResult.Errors.FirstOrDefault()?.ErrorMessage;
+            return handlerResponse;
+        }
+
+        var user = _mapper.Map<User>(request);
+        var userRepository = _unitOfWork.GetRepository<User, Guid>();
+        await userRepository?.UpdateAsync(user, cancellationToken)!;
+
+        handlerResponse.IsSucceed = true;
+        handlerResponse.Data = _mapper.Map<UpdateUserCommandResponse>(request);
+        handlerResponse.Message = Features.User.Commands.Update.Handler.HandlerConsts.HandlerIsSucceedMessage;
+
+        return handlerResponse;
+    }
+
+    #endregion
+}
---
+++ Cucina.Application/Features/User/Commands/Update/Model/Request/UpdateUserCommandRequest.cs
@@ -0,0 +1,9 @@
+﻿using Cucina.Application.Features.Common;
+using MediatR;
+
+namespace Cucina.Application;
+
+public class UpdateUserCommandRequest : IRequest<HandlerResponse<UpdateUserCommandResponse>>
+{
+    public Guid Id { get; set; }
+}
