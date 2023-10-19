54106035489f929ac0709eb9aee94f8beab74085
Mohammadreza Khoshghamat <58995528+MrKhoshghamat@users.noreply.github.com>
2023-10-19 13:06:23 +0330

    UnitOfWork Added and Developed

---
+++ Cucina.Domain/Contracts/Base/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Cucina.Domain.Entities.Base;
+
+namespace Cucina.Domain;
+
+public interface IUnitOfWork : IAsyncDisposable, IDisposable
+{
+    IRepository<TEntity, TPrimaryKey>? GetRepository<TEntity, TPrimaryKey>() where TEntity : BaseEntity<TPrimaryKey>;
+    Task SaveChangesAsync();
+    void SaveChanges();
+}
